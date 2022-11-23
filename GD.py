9f064b03623a0812809ea8097b3f41989ca89ea3
powcoder <powcoder@163.com>
2022-11-23 13:08:19 +0800

    python gradient ha2

---
+++ GD.py
@@ -0,0 +1,38 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+
+import numpy as np
+# X          - single array/vector
+# y          - single array/vector
+# theta      - single array/vector
+# alpha      - scalar
+# iterations - scarlar
+
+def gradientDescent(X, y, theta, alpha, numIterations):
+    '''
+    # This function returns a tuple (theta, Cost array)
+    '''
+    m = len(y)
+    arrCost =[];
+    transposedX = np.transpose(X) # transpose X into a vector  -> XColCount X m matrix
+    for interation in range(0, numIterations):
+        ################PLACEHOLDER3 #start##########################
+        #: write your codes to update theta, i.e., the parameters to estimate. 
+	# Replace the following variables if needed
+        residualError =  
+        gradient =  
+        change = [alpha * x for x in gradient]
+        theta = np.subtract(theta, change)  # or theta = theta - alpha * gradient
+        ################PLACEHOLDER3 #end##########################
+
+        ################PLACEHOLDER4 #start##########################
+        # calculate the current cost with the new theta; 
+        atmp =    
+	print(atmp)
+	arrCost.append(atmp)
+        # cost = (1 / m) * np.sum(residualError ** 2)
+        ################PLACEHOLDER4 #start##########################
+
+    return theta, arrCost
